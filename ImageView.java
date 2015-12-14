134839d03f57f55a68c8cae093f30d4df7d52e03
brandonpillaylee <brandonpillaylee@users.noreply.github.com>
2015-12-14 12:01:16 -0800

    Main.java

--- ImageView.java
+++ ImageView.java
@@ -1,29 +1,44 @@
+package application;
+	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;   
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
-public class ImageView extends Application 
-{ 
-   public void start(Stage stage) throws Exception
-   {  
-      // Load the FXML file.
-      Parent parent = FXMLLoader.load(
-               getClass().getResource("ImageViewDemo.fxml")); 
-      
-      // Build the scene graph.
-      Scene scene = new Scene(parent); 
-      
-      // Display our window, using the scene graph.
-      stage.setTitle("ImageViewDemo"); 
-      stage.setScene(scene);
-      stage.show(); 
-   }
-   
-   public static void main(String[] args)
-   {
-      // Launch the application.
-      launch(args);
-   }
- } 
+
+public class Main extends Application {
+	public Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+	this.primaryStage.setTitle("Image Gallery");
+	try {
+        //Initialize stage
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("ImageView.fxml"));
+        AnchorPane imageOverview = (AnchorPane) loader.load();
+        
+        
+        //Connect to FXML
+        ImageViewController controller = loader.getController();
+        controller.setPrevStage(primaryStage);
+        
+        Scene scene = new Scene(imageOverview, 700, 600);
+        primaryStage.setScene(scene);
+			
+		primaryStage.show();
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+
+public Stage getPrimaryStage() {
+    return primaryStage;
+}
+
+public static void main(String[] args) {
+	launch(args);
+}
+}
